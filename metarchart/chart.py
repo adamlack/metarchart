eee5b0b15c74b7af3047446e9c767ec4ab359ef1
Adam Lack <git@adamlack.com>
2020-07-06 15:07:11 +0100

    initial layout and styling

--- metarchart/chart.py
+++ metarchart/chart.py
@@ -13,13 +13,22 @@ def index():
     icao = request.args.get('icao')
     if icao == None:
         icao = 'EGVO'
-    time_window = int(request.args.get('h'))
+    time_window = request.args.get('h')
+    if time_window == None:
+        time_window = 12
+    else:
+        time_window = int(time_window)
     metar_data = latestMetars(icao, time_window)
-    name, units, values, times = extract(metar_data, 'wgust')
+    name, units, values, times = extract(metar_data, 'wspeed')
     data={}
     data['Time'] = times
     data[name] = values
 
     script, div = make_plot.timeLineChart(data, "Time", name, units, icao)
 
-    return render_template('chart/index.html', icao=icao, the_div=div, the_script=script)
+    return render_template(
+        'chart/index.html',
+        details={'icao':icao, 'name':name, 'units':units, 'time_window':time_window},
+        the_div=div,
+        the_script=script
+    )
