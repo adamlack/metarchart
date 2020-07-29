4a11576a269e81473d00c7bdbaa72022892f1912
Adam Lack <git@adamlack.com>
2020-07-29 13:30:44 +0100

    updated setup dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import find_packages, setup
 
 setup(
     name='metarchart',
-    version='0.0.1',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'bokeh',
+        'numpy',
+        'requests',
+        'metar',
+        'flask_wtf',
+        'wtforms',
+        '',
+        '',
+        '',
+        '',
     ],
 )
